563e08434fb688703d58a49929c904cf52e1d92e
Shohijahon <sodiqjonovshohijahon2@gmail.com>
2023-11-13 14:49:50 +0500

    zakaz_telegram_bot

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Shohijahon\Desktop\zakaz_telegram_bot\env
+set VIRTUAL_ENV=C:\Users\Maxtel\Desktop\zakaz_telegram_bot\env
 
 if not defined PROMPT set PROMPT=$P$G
 
