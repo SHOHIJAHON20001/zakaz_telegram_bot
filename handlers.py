a5040fd6f953b81e50ae8fb565864eebc8a0c86e
Shohijahon <sodiqjonovshohijahon2@gmail.com>
2023-11-13 11:31:26 +0500

    zakaz_telegram_bot

--- handlers.py
+++ handlers.py
@@ -6,7 +6,6 @@ from aiogram import Bot, Dispatcher, types
 from aiogram import F
 from aiogram.enums import ParseMode
 from aiogram.filters import Command 
-from aiogram.types import Message 
 from aiogram.utils.markdown import hbold
 from aiogram.utils.keyboard import ReplyKeyboardMarkup, KeyboardButton
 from config import TOKEN
@@ -28,28 +27,34 @@ async def start(message: types.Message):
 
 @dp.message(F.text == "Rangli chop etish")
 async def rangli(message: types.Message):
-    # Create a ReplyKeyboardMarkup with the "Ortga" button
     ortga_kb = ReplyKeyboardMarkup(
         keyboard=[[KeyboardButton(text="Ortga")]],
         resize_keyboard=True,
     )
-    await message.answer("Rangsiz printer 100 sumdan hisoblaydi har bir varoq uchun. Varoqlar sonini kiriting (faqat raqam kiriting): ", reply_markup=ortga_kb)
+    await message.answer("1 varoqning narxi 100 sumdan. Varoqlar sonini kiriting: ", reply_markup=ortga_kb)
 
-@dp.message(F.text == "Umumiy")
-async def umumiy(message: types.Message):
-    # Assuming cost_per_varoq is the cost returned from the rangli function
+@dp.message(F.text.isdigit())
+async def umumiy_rangli(message: types.Message):
     cost_per_varoq = 200 
-
     try:
-        varoq_count = int(message.text)  # Assuming the user enters the number of varoqs
+        varoq_count = int(message.text)
         total_cost = cost_per_varoq * varoq_count
         await message.answer(f"Umumiy narxi: {total_cost}")
-    except ValueError:
-        await message.answer("Varoq sonini raqam bilan kiriting.")
+    except:
+        await message.answer("Iltimos faqat raqam kiriting!")
+
+
+@dp.message(F.text == "Rangsiz chop etish")
+async def rangsiz(message: types.Message):
+    ortga = ReplyKeyboardMarkup(
+        keyboard=[[KeyboardButton(text="Ortga")]],
+        resize_keyboard=True
+    )
+    await message.answer("Bir varoqning narxi 100 sumdan. Varoqlar sonini kiriting: ", reply_markup=ortga)
+
 
 @dp.message(F.text == "Ortga")
 async def ortga(message: types.Message):
-    # Handle the "Ortga" button press
     kb = (
         [types.KeyboardButton(text="Rangli chop etish")],
         [types.KeyboardButton(text="Rangsiz chop etish")]
@@ -60,13 +65,6 @@ async def ortga(message: types.Message):
     )
     await message.answer("Bosh menu", reply_markup=keyboard)
 
-@dp.message(F.text == "Rangsiz chop etish")
-async def rangsiz(message: types.Message):
-    ortga = ReplyKeyboardMarkup(
-        keyboard=[[KeyboardButton(text="Ortga")]],
-        resize_keyboard=True
-    )
-    await message.answer("Rangsiz printer uchun 200 sumdan hisoblanadi. Varoqlar sonini kiriting (faqat raqam kiriting): ", reply_markup=ortga)
 
 async def main():
     bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
